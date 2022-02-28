9681b99af6f6bd2b956c08d7e062bf33a1f1b0c9
vollcheck <j3k.walczak@gmail.com>
2022-02-28 17:17:58 +0100

    rewrite it using generators and OOP

--- pylan.py
+++ pylan.py
@@ -1,5 +1,5 @@
 from datetime import datetime, date, timedelta
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Callable, Iterable
 from io import StringIO
 from csv import DictReader
 
@@ -8,90 +8,84 @@ from rich.console import Console
 from rich.table import Table
 
 
-Plan = List[Dict[str, str]]
+# Constants
+URL = ("https://wzr.ug.edu.pl/.csv/plan_st.php?f1=N22-32"
+       "&f2=4&jp=cf4f962e1fd3c99dd511843f647d568fb7957663")
 
 
-URL = "https://wzr.ug.edu.pl/.csv/plan_st.php?f1=N22-32&f2=4&jp=cf4f962e1fd3c99dd511843f647d568fb7957663"
-session = CachedSession(
-    "simple_cache",
-    use_cache_dir=True,  # Save files in the default user cache dir
-    cache_control=True,  # Use Cache-Control headers for expiration, if available
-    expire_after=timedelta(days=7),  # Otherwise expire responses after one day
-    allowable_methods=[
-        "GET",
-        "POST",
-    ],  # Cache POST requests to avoid sending the same data twice
-    allowable_codes=[
-        200,
-        400,
-    ],  # Cache 400 responses as a solemn reminder of your failures
-    # ignored_parameters=["api_key"],  # Don't match this param or save it in the cache
-    match_headers=True,  # Match all request headers
-    stale_if_error=True,  # In case of request errors, use stale cache data if possible
-)
+class PlanExplorer:
+    Unit = Dict[str, str]
+    Plan = Iterable[Unit]
 
+    def __init__(self, url: str, use_cache: bool = False):
+        if use_cache:
+            self.session = self.create_session()
+            response = self.session.get(url)
 
-def download(url: str) -> Optional[Plan]:
-    response = session.get(url)
-    if response.status_code != 200:
-        return None
+        else:
+            import requests
+            response = requests.get(url)
 
-    reader = DictReader(StringIO(response.text))
+        if response.status_code != 200:
+            return None
 
-    # since DictReader is an iterator, not iterable,
-    # we need to copy it to another structure
-    return [row for row in reader]
+        self.data = self._process_data(response)
 
+    def _process_data(self, response) -> Plan:
+        reader = DictReader(StringIO(response.text))
+        return (self.process_unit(unit) for unit in reader)
 
-def process(plan: Plan) -> Plan:
-    today = date.today()
-    dates: List[str] = [d["Start Date"] for d in plan]
+    @staticmethod
+    def create_session() -> CachedSession:
+        return CachedSession(
+            "simple_cache",
+            use_cache_dir=True,  # Save files in the default user cache dir
+            cache_control=True,  # Use Cache-Control headers for expiration, if available
+            expire_after=timedelta(days=3),  # Otherwise expire responses after one day
+            allowable_methods=[
+                "GET",
+                "POST",
+            ],  # Cache POST requests to avoid sending the same data twice
+            allowable_codes=[
+                200,
+                400,
+            ],  # Cache 400 responses as a solemn reminder of your failures
+            # ignored_parameters=["api_key"],  # Don't match this param or save it in the cache
+            match_headers=True,  # Match all request headers
+            stale_if_error=True,  # In case of request errors, use stale cache data if possible
+        )
 
-    processed_dates: List[date] = [
-        datetime.strptime(d, "%m/%d/%Y").date() for d in dates
-    ]
+    @staticmethod
+    def process_unit(unit) -> Unit:
+        # "%m/%d/%Y %H.%M"
+        return {
+            "sub": unit['Subject'],
+            "date": unit['Start Date'],
+            "time": f"{unit['Start Time']} - {unit['End Time']}",
+            "loc": unit["Location"].split(",", 1)[0]
+        }
 
-    deltas: List[timedelta] = [d - today for d in processed_dates]
+    def present(self, filtering: Optional[Callable] = None) -> None:
+        console = Console()
+        table = Table(show_header=True)  # header_style
 
-    min_delta = min(deltas)
-    min_delta_inc = min(deltas) + timedelta(days=1)
+        for header in ["Subject", "Date", "Time", "Location"]:
+            table.add_column(header)
 
-    next_weekend_dates: List[timedelta] = [
-        d for d in deltas if d == min_delta or d == min_delta_inc
-    ]
-    next_dates = set(dates[: len(next_weekend_dates)])
+        # filtering part
+        if filtering:
+            data = (u for u in self.data if filtering(u))
+        else:
+            data = self.data
 
-    return [d for d in plan if d["Start Date"] in next_dates]
+        for row in data:
+            table.add_row(*row.values())
 
-
-def present(plan: Plan) -> None:
-    console = Console()
-
-    for row in plan:
-        row["Date"] = row["Start Date"]
-        row["Lecturer"] = row["Description"][7:]
-        del row["Description"]
-        del row["Start Date"]
-        del row["End Date"]
-        del row["Location"]
-
-    table = Table(show_header=True)  # header_style
-
-    headers = plan[0].keys()
-
-    for header in headers:
-        table.add_column(header)
-
-    for row in plan:
-        table.add_row(*row.values())
-
-    console.print(table)
+        console.print(table)
 
 
 if __name__ == "__main__":
-    plan: Optional[Plan] = download(URL)
-    if plan:
-        processed: Plan = process(plan)
-        present(processed)
-    else:
-        print("Plan hasn't been downloaded")
+    plan = PlanExplorer(URL)
+    plan.present(
+        lambda u: u['date'].split()[0] == "03/12/2022"
+    )
